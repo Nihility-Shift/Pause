53145e3fee3677c22223acddc6c89e0176b2ecb6
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-11-02 19:36:35 -0700

    Fixed Power Reference

--- Pause/PauseManager.cs
+++ Pause/PauseManager.cs
@@ -11,7 +11,7 @@ using System.Linq;
 using System.Collections.Generic;
 using Gameplay.Enhancements;
 using CG.Ship.Modules;
-using Gameplay.PowerSystem;
+using Gameplay.Power;
 
 namespace Pause
 {
