f123fb92010993888174db525e2241f9bfb3a827
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-12-13 10:53:34 -0800

    Changed UI to utilize timescaled time instead of real time

---
+++ Pause/HudTimersPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Pause
+{
+    //Uses real time since start, which may be nice for certain UI elements while paused, but is terrible for the Void Jump Stability number.
+    [HarmonyPatch("UnityEngine.UIElements.Panel, UnityEngine.UIElementsModule, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null", "DefaultTimeSinceStartupMs")]
+    internal class HudTimersPatch
+    {
+        static bool Prefix(ref long __result)
+        {
+            __result = (long)(Time.time * 1000);
+            return false;
+        }
+    }
+}
